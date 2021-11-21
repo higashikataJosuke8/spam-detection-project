5a7c872e9e4fd006c6309e489ea70b8ac0fab703
devbcdestiller <dev.bcdestiller@gmail.com>
2021-11-21 18:26:17 +0800

    Added RNN model class file

---
+++ Web App/test.py
@@ -0,0 +1,7 @@
+from model.rnn_model import RNNModel
+
+pred_text = "ATE/KUYA sa inyo ko po napadala yung LOAD na 300 pang emergency LOAD po kasi yun Baka po pwedeng pakibalik gawin nyo na lang po iText 300 SEND nyo po sa 2965550200"
+
+model = RNNModel('model/rnn-sms')
+
+print(model.get_prediction(pred_text))
