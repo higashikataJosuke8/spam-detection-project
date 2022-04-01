620d0537e197dbc7acc4697927a56ac82772c3f5
Dev.Estiller <68733016+devbcdestiller@users.noreply.github.com>
2022-04-01 10:08:45 +0800

    Delete test.py

--- Web App/test.py
+++
@@ -1,7 +0,0 @@
-from model.rnn_model import RNNModel
-
-pred_text = "Subject:  New jobs similar to Information Technology Intern at foodpanda"
-
-model = RNNModel('model/rnn-mail')
-
-print(model.get_prediction(pred_text))
